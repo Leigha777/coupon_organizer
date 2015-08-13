5c19ef004a2e528d1cea4ded5956b182e951ee79
Cindy <cindyzhou131@gmail.com>
2015-08-13 15:10:37 +0000

    ruby file update

--- models/coupon.rb
+++ models/coupon.rb
@@ -1,17 +1,26 @@
-require 'date'
+# require 'date'
 class Coupon
     @@coupon_just_names = []
     @@coupon_by_name = {}
     @@coupon_by_store = {}
-    @@coupon_by_expiraton = {}
+    @@coupon_by_expiration = {}
     @@coupon_by_discount = {
        :percent => {},
        :dollars_off => {},
        :miscellaneous => {} # for like buy one get one free or stuff like that
          }
-   @@coupon_by_discount_only = {} #i think it would be better to organize it by the type of discount but i don't know how to order that by amount
+   @@coupon_by_type = {
+       :food => {},
+       :clothing => {},
+       :electronics => {},
+       :supplies => {},
+       :funiture => {},
+       :other => {}
+      }
    
    attr_reader :type, :store, :discount_amount, :expiration_date, :alphabetical, :by_store, :discount, :expiration,
+   
+   
    def Coupon.list
       @@coupon_just_names
    end
@@ -21,12 +30,10 @@ class Coupon
    def Coupon.coupon_by_store
       @@coupon_by_store
    end
-   def Coupon.coupon_by_expiraton
-      return @@coupon_by_expiraton
+   def Coupon.coupon_by_expiration
+      return @@coupon_by_expiration
    end
-   def Coupon.coupon_by_discount_only
-      @@coupon_by_discount_only
-   end
+
    
    
    def Coupon.coupon_by_discount
@@ -37,41 +44,47 @@ class Coupon
 #       string = @@coupon_by_discount[:percent].sort_by { |discount, info| discount }
       @@coupon_by_discount[:percent].each do |discount, info| #why doesn't this print everything out? 
          return "Percent: " + discount.to_s + "=" + info
-      end
-      
+      end    
    end
    
-   def Coupon.sort
-      @@coupon_by_discount_only.sort_by { |discount, info| discount } #deletes things that share the same value and only keeps one of them
-   end
    def Coupon.sort1
       @@coupon_by_discount[:percent].sort_by { |discount, info| discount }
+   end
+   def Coupon.ex
+      @@coupon_by_expiration.sort_by { |date, info| date }
    end
    def Coupon.alphabet
       @@coupon_by_name.sort_by { |name, info| name }
    end #AHHHH IT WORKS THIS IS FOR SUREEE
    
-   def initialize(name, store, discount_amount, expiration_date, description = "none")
+      def initialize(name, store, discount_amount, expiration_date, type, description = "none")
       @name = name
       @store = store 
       @discount_amount = discount_amount
       @expiration_date = expiration_date
+      @type = type
       @description = description
-#       @@coupon_by_name << @name
-#       @@coupon_by_store << @store
-#       @@coupon_by_expiraton << @expiration_date
-#       if discount_amount.include? "%"
-#          @@coupon_by_discount[:percent] << @discount_amount
-#       elsif discount_amount.include? "$"
-#          @@coupon_by_discount[:dollars_off] << @discount_amount
-#       else
-#          @@coupon_by_discount[:miscellaneous] << @discount_amount
-#       end
+         
       @@coupon_just_names << name
       @@coupon_by_name[name] = display_everything
       @@coupon_by_store[store] = display_everything
-      @@coupon_by_expiraton[expiration_date] = display_everything
-      @@coupon_by_discount_only[discount_amount.to_f] = display_everything
+      @@coupon_by_expiration[expiration_date] = display_everything
+      
+      
+      if type == "Food"
+         @@coupon_by_type[:food][type] = display_everything
+      elsif type == "Clothings"
+         @@coupon_by_type[:clothing][type] = display_everything
+      elsif type == "Electronics"
+         @@coupon_by_type[:electronics][type] = display_everything
+      elsif type == "Office Supplies"
+         @@coupon_by_type[:supplies][type] = display_everything
+      elsif type == "Furniture"
+         @@coupon_by_type[:furniture][type] = display_everything
+      else
+         @@coupon_by_type[:other][type] = display_everything
+      end
+      
       if discount_amount.include? "%"
          @@coupon_by_discount[:percent][discount_amount.to_f] = display_everything
       elsif discount_amount.include? "$"
@@ -82,6 +95,7 @@ class Coupon
       end
       
    end
+   
    def organization(type)
    if type == "discount"
       @@coupon_by_discount.sort_by { |k, v| v[:percent] }
@@ -93,12 +107,13 @@ end
   Store: #{@store}
   Discount Amount: #{@discount_amount}
   Expiration Date: #{@expiration_date}
+  Type: #{@type}
   Other Discriptions: #{@description}"
    end
-   def make_a_date
-      @expiration_date = @@date
-      @@date = Date.parse
-   end
+#    def make_a_date
+#       @expiration_date = @@date
+#       @@date = Date.parse
+#    end
 
    
 
@@ -106,14 +121,12 @@ end
 end
 
 
-Michaels = Coupon.new("paint", "Michaels", "20%", "3 Feb 2014")
-#puts Michaels.display_everything
-Target = Coupon.new("dogs", "Target", "$3", "April") #why is this displayed as a ZERO?????? 
-Best = Coupon.new("best", "bestbuy", "4%", "May")
-Waywward = Coupon.new("nice", "Wa", "$13", "Tomorrow")
-Awall = Coupon.new("aayywa", "Craycray", "12%", "Monday")
-#Target.compiling("Discount (dollars)")
-#Michaels.compiling("Discount (percent)")
+Michaels = Coupon.new("paint", "Michaels", "20%", "1997/02/09", "Food")
+Target = Coupon.new("dogs", "Target", "$3", "2007/02/09", "Other") #why is this displayed as a ZERO?????? 
+Best = Coupon.new("best", "bestbuy", "4%", "2019/12/2", "Other")
+Waywward = Coupon.new("nice", "Wa", "$13", "1997/12/02", "Electronics")
+Awall = Coupon.new("aayywa", "Craycray", "12%", "1923/12/03", "Office Supplies")
+
 #puts Coupon.coupon_by_name
 #puts Coupon.coupon_by_name
 # puts Target.expiration_date
@@ -121,9 +134,5 @@ Awall = Coupon.new("aayywa", "Craycray", "12%", "Monday")
 #Coupon.organization("discount")
 #puts Coupon.coupon_by_discount
 # puts Coupon.sort1
-######puts Coupon.alphabet
-
-d = Date.parse('3rd Feb 2001')
-puts d.year
-puts Michaels.make_a_date.year
-
+#puts Coupon.alphabet
+puts Coupon.ex
